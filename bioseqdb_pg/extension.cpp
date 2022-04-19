899eb762c5808534d6277c57de78f887a3dad48b
Mateusz Cegiełka <mateusz@cegla.net>
2022-04-19 20:51:31 +0200

    Add basic set-valued functions support (#3)
    * Add basic nucleotide sequence support
    
    * Add basic extension installation
    
    * Add simpler CI
    
    * Fix C++17 support
    
    * Add example of ValuePerCall mode
    
    * Add example of Materialize mode
    
    * Add example of returning a nucleotide sequence

--- bioseqdb_pg/extension.cpp
+++ bioseqdb_pg/extension.cpp
@@ -1,9 +1,14 @@
 #include <algorithm>
+#include <chrono>
+#include <random>
+#include <string>
 #include <string_view>
 
 extern "C" {
 #include <postgres.h>
 #include <fmgr.h>
+#include <funcapi.h>
+#include <miscadmin.h>
 }
 
 struct PgNucleotideSequence {
@@ -86,6 +91,100 @@ Datum nuclseq_complement(PG_FUNCTION_ARGS) {
         }
     }
     PG_RETURN_POINTER(complement);
+}
+
+PG_FUNCTION_INFO_V1(yoyo_v1);
+Datum yoyo_v1(PG_FUNCTION_ARGS) {
+    if (SRF_IS_FIRSTCALL()) {
+        FuncCallContext* funcctx = SRF_FIRSTCALL_INIT();
+        MemoryContext oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+        int num_tuples = PG_GETARG_INT32(0);
+        if (num_tuples < 0) {
+            ereport(ERROR,
+                    (errcode(ERRCODE_INVALID_PARAMETER_VALUE)),
+                    errmsg("number of rows cannot be negative"));
+        }
+        funcctx->max_calls = num_tuples;
+
+        MemoryContextSwitchTo(oldcontext);
+    }
+
+    FuncCallContext* funcctx = SRF_PERCALL_SETUP();
+    uint64_t result = funcctx->call_cntr;
+    if (funcctx->call_cntr < funcctx->max_calls) {
+        SRF_RETURN_NEXT(funcctx, UInt64GetDatum(result));
+    } else {
+        SRF_RETURN_DONE(funcctx);
+    }
+}
+
+PG_FUNCTION_INFO_V1(yoyo_v2);
+Datum yoyo_v2(PG_FUNCTION_ARGS) {
+    ReturnSetInfo* rsi = reinterpret_cast<ReturnSetInfo*>(fcinfo->resultinfo);
+    if (rsi == NULL || !IsA(rsi, ReturnSetInfo)) {
+        ereport(ERROR,
+                (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                errmsg("set-valued function called in context that cannot accept a set")));
+    }
+    if (!(rsi->allowedModes & SFRM_Materialize)) {
+        ereport(ERROR,
+                (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                errmsg("materialize mode required, but it is not allowed in this context")));
+    }
+
+    MemoryContext per_query_ctx = rsi->econtext->ecxt_per_query_memory;
+
+    TupleDesc tupdesc;
+    switch (get_call_result_type(fcinfo, nullptr, &tupdesc)) {
+        case TYPEFUNC_COMPOSITE:
+            break;
+        case TYPEFUNC_RECORD:
+            ereport(ERROR,
+                    (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                    errmsg("function returning record called in context that cannot accept type record")));
+            break;
+        default:
+            ereport(ERROR,
+                    (errcode(ERRCODE_DATATYPE_MISMATCH),
+                    errmsg("return type must be a row type")));
+            break;
+    }
+
+    MemoryContext old_ctx = MemoryContextSwitchTo(per_query_ctx);
+    tupdesc = CreateTupleDescCopy(tupdesc);
+    Tuplestorestate* tupstore = tuplestore_begin_heap(rsi->allowedModes & SFRM_Materialize_Random, false, work_mem);
+    MemoryContextSwitchTo(old_ctx);
+
+    AttInMetadata* attr_input_meta = TupleDescGetAttInMetadata(tupdesc);
+
+    std::minstd_rand rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+    for (int i=0; i<6; ++i) {
+        std::string alpha = std::to_string(i + 1);
+        std::string beta = std::to_string((i + 1) * (i + 1));
+        std::string gamma;
+        for (int j=0; j<20; ++j) {
+            gamma += "ACGT"[std::uniform_int_distribution<int>(0, 3)(rng)];
+        }
+        alpha.c_str();
+        beta.c_str();
+        gamma.c_str();
+
+        char* values[3];
+        values[0] = alpha.data();
+        values[1] = beta.data();
+        values[2] = gamma.data();
+
+        HeapTuple tuple = BuildTupleFromCStrings(attr_input_meta, values);
+        tuplestore_puttuple(tupstore, tuple);
+        heap_freetuple(tuple);
+    }
+
+    rsi->returnMode = SFRM_Materialize;
+    rsi->setResult = tupstore;
+    rsi->setDesc = tupdesc;
+    return (Datum) nullptr;
 }
 
 }
